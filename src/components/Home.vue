cced62c4f4b062fdf9236b856e4d58ed6355b3d0
Gregor Laan <gregor@github.ee>
2018-07-29 15:04:13 +0300

    Test chatkit API connection

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,8 +9,28 @@ import { ChatManager, TokenProvider } from '@pusher/chatkit'
 
 export default {
   data () {
-    return {
+    return {}
+  },
+  methods: {
+    onLoad: function () {
+      console.log('App loaded.')
+      const chatManager = new ChatManager({
+        instanceLocator: 'v1:us1:811af568-f6dc-4eb4-bed1-c842ac6cdb78',
+        userId: 'gregor.laan',
+        tokenProvider: new TokenProvider({ url: 'https://us1.pusherplatform.io/services/chatkit_token_provider/v1/811af568-f6dc-4eb4-bed1-c842ac6cdb78/token' })
+      })
+
+      chatManager.connect()
+        .then(currentUser => {
+          console.log('Successful connection', currentUser)
+        })
+        .catch(err => {
+          console.log('Error on connection', err)
+        })
     }
+  },
+  mounted: function () {
+    this.onLoad()
   }
 }
 </script>
